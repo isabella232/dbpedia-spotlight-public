6f0972ceed4dd0959239f4bf0cd22aa2ed069bd9
akshay <akshay>
2016-06-06 17:40:25 +0100

    Additional logging for clarity on stemmers, spotters, tokenizers

--- core/src/main/scala/org/dbpedia/spotlight/db/SpotlightModel.scala
+++ core/src/main/scala/org/dbpedia/spotlight/db/SpotlightModel.scala
@@ -1,25 +1,28 @@
 package org.dbpedia.spotlight.db
 
-import concurrent.{TokenizerWrapper, SpotterWrapper}
+import concurrent.{SpotterWrapper, TokenizerWrapper}
 import org.dbpedia.spotlight.db.memory.{MemoryContextStore, MemoryStore}
 import model._
-import opennlp.tools.tokenize.{TokenizerModel, TokenizerME}
-import opennlp.tools.sentdetect.{SentenceModel, SentenceDetectorME}
+import opennlp.tools.tokenize.{TokenizerME, TokenizerModel}
+import opennlp.tools.sentdetect.{SentenceDetectorME, SentenceModel}
 import opennlp.tools.postag.{POSModel, POSTaggerME}
 import org.dbpedia.spotlight.disambiguate.mixtures.UnweightedMixture
-import org.dbpedia.spotlight.db.similarity.{NoContextSimilarity, GenerativeContextSimilarity, ContextSimilarity}
+import org.dbpedia.spotlight.db.similarity.{ContextSimilarity, GenerativeContextSimilarity, NoContextSimilarity}
+
 import scala.collection.JavaConverters._
 import org.dbpedia.spotlight.model.SpotterConfiguration.SpotterPolicy
 import org.dbpedia.spotlight.model.SpotlightConfiguration.DisambiguationPolicy
 import org.dbpedia.spotlight.disambiguate.ParagraphDisambiguatorJ
 import org.dbpedia.spotlight.spot.{SpotXmlParser, Spotter}
-import java.io.{IOException, File, FileInputStream}
+import java.io.{File, FileInputStream, IOException}
 import java.util.{Locale, Properties}
+
 import opennlp.tools.chunker.ChunkerModel
 import opennlp.tools.namefind.TokenNameFinderModel
 import stem.SnowballStemmer
-import tokenize.{OpenNLPTokenizer, LanguageIndependentTokenizer}
+import tokenize.{LanguageIndependentTokenizer, OpenNLPTokenizer}
 import org.dbpedia.spotlight.exceptions.ConfigurationException
+import org.dbpedia.spotlight.log.SpotlightLog
 import org.dbpedia.spotlight.util.MathUtil
 
 
@@ -80,7 +83,9 @@ object SpotlightModel {
     //Load the stemmer from the model file:
     def stemmer(): Stemmer = properties.getProperty("stemmer") match {
       case s: String if s equals "None" => null
-      case s: String => new SnowballStemmer(s)
+      case s: String =>
+        SpotlightLog.info(this.getClass, "Using SnowballStemmer for stemming")
+        new SnowballStemmer(s)
     }
 
     def contextSimilarity(): ContextSimilarity = contextStore match {
@@ -107,13 +112,14 @@ object SpotlightModel {
         tokenTypeStore
       ).asInstanceOf[TextTokenizer]
 
+      SpotlightLog.info(this.getClass, "Using org.dbpedia.spotlight.db.tokenize.OpenNLPTokenizer")
       if(cores.size == 1)
         createTokenizer()
       else
         new TokenizerWrapper(cores.map(_ => createTokenizer())).asInstanceOf[TextTokenizer]
-
     } else {
       val locale = properties.getProperty("locale").split("_")
+      SpotlightLog.info(this.getClass, "Using org.dbpedia.spotlight.db.tokenize.LanguageIndependentTokenizer for locale " + locale)
       new LanguageIndependentTokenizer(stopwords, stemmer(), new Locale(locale(0), locale(1)), tokenTypeStore)
     }
 
@@ -147,6 +153,7 @@ object SpotlightModel {
         Some(loadSpotterThresholds(new File(modelFolder, "spotter_thresholds.txt")))
       ).asInstanceOf[Spotter]
 
+      SpotlightLog.info(this.getClass, "Selecting OpenNLP spotter refer org.dbpedia.spotlight.db.OpenNLPSpotter")
       if(cores.size == 1)
         createSpotter()
       else
@@ -156,6 +163,7 @@ object SpotlightModel {
 
     } else {
       val dict = MemoryStore.loadFSADictionary(new FileInputStream(new File(modelFolder, "fsa_dict.mem")))
+      SpotlightLog.info(this.getClass, "Using the FSASpotter refer org.dbpedia.spotlight.db.FSASpotter")
 
       new FSASpotter(
         dict,
